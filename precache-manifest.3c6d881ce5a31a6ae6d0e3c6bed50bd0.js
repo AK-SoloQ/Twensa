95d006182d480e86dcac23f25cc9bf9d48ade663
abdelmoula ahmed <ahmed.abdelmoula@outlook.fr>
2019-02-03 23:24:27 +0100

    Updates

---
+++ precache-manifest.3c6d881ce5a31a6ae6d0e3c6bed50bd0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "80adda5d1367007773f9",
+    "url": "./static/css/main.1bfc16f7.chunk.css"
+  },
+  {
+    "revision": "80adda5d1367007773f9",
+    "url": "./static/js/main.80adda5d.chunk.js"
+  },
+  {
+    "revision": "2e0fe42fd2fa00982647",
+    "url": "./static/css/1.167e137e.chunk.css"
+  },
+  {
+    "revision": "2e0fe42fd2fa00982647",
+    "url": "./static/js/1.2e0fe42f.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "6e846951d7a04c931a0f9efa94a1316f",
+    "url": "./static/media/5.6e846951.jpeg"
+  },
+  {
+    "revision": "47789d678cfff97c48fda4de3f3621d5",
+    "url": "./static/media/6.47789d67.jpeg"
+  },
+  {
+    "revision": "73476783ca0f7583198978de7073815b",
+    "url": "./static/media/0.73476783.jpg"
+  },
+  {
+    "revision": "ed986061ad9bf89c081724362956b134",
+    "url": "./static/media/1.ed986061.jpeg"
+  },
+  {
+    "revision": "780ccb41c83dad3cbd24a9bcd3262586",
+    "url": "./static/media/2.780ccb41.jpeg"
+  },
+  {
+    "revision": "41bf3241965cc7c2990174ed9d6e9714",
+    "url": "./static/media/3.41bf3241.jpeg"
+  },
+  {
+    "revision": "026cb7af3133d721efff2a0134f7e16f",
+    "url": "./static/media/4.026cb7af.jpeg"
+  },
+  {
+    "revision": "a6f3dd467c49003b0c84180357c2029c",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3bf7e93d304ca3ffe78003fb5bfa17f1.js"
+  "./precache-manifest.3c6d881ce5a31a6ae6d0e3c6bed50bd0.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
