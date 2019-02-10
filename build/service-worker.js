74a88b0f0d4b55c7a7e3355a4fe2843d89374622
abdelmoula ahmed <ahmed.abdelmoula@outlook.fr>
2019-02-10 02:28:22 +0100

    version 0.0.23

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.3c6d881ce5a31a6ae6d0e3c6bed50bd0.js"
+  "./precache-manifest.9288c221d1208b16d5c55196fadb488b.js"
 );
 
 workbox.clientsClaim();
