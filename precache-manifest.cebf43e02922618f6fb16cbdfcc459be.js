98ce220b0b9acffaafe3598a5861c18a3d86c314
abdelmoula ahmed <ahmed.abdelmoula@outlook.fr>
2019-04-07 23:54:28 +0100

    Updates

--- precache-manifest.cebf43e02922618f6fb16cbdfcc459be.js
+++ precache-manifest.cebf43e02922618f6fb16cbdfcc459be.js
@@ -1,30 +1,30 @@
 self.__precacheManifest = [
   {
-    "revision": "996167029382e1151e81",
+    "revision": "ee5358b8cdc01aa2da93",
     "url": "./static/css/main.4374adb4.chunk.css"
   },
   {
-    "revision": "996167029382e1151e81",
-    "url": "./static/js/main.14b09f5e.chunk.js"
+    "revision": "ee5358b8cdc01aa2da93",
+    "url": "./static/js/main.d37ddf68.chunk.js"
   },
   {
     "revision": "8c97409f0ee389fe75da",
     "url": "./static/js/runtime~main.d653cc00.js"
   },
   {
-    "revision": "c9f1f3aec61ba8af9880",
+    "revision": "be538e9f32e91c5aa2d0",
     "url": "./static/css/2.88e191cb.chunk.css"
   },
   {
-    "revision": "c9f1f3aec61ba8af9880",
-    "url": "./static/js/2.83eb6ac2.chunk.js"
+    "revision": "be538e9f32e91c5aa2d0",
+    "url": "./static/js/2.4e1c74cf.chunk.js"
   },
   {
     "revision": "73476783ca0f7583198978de7073815b",
     "url": "./static/media/0.73476783.jpg"
   },
   {
-    "revision": "780e0f5e189387e8d3ff08ea6b854d67",
+    "revision": "ed4cea615ea632e8432e08277dc99842",
     "url": "./index.html"
   }
 ];
